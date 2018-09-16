5099e2ec9f2d0b25a8768512ac472358e1c52bea
Sergey Malyshev <malyshev.s.nick@gmail.com>
2018-09-16 22:22:55 +0300

    change the head of the symfony app

--- task.sh
+++ task.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 npm run build;
-cp /var/www/react-app/dist/base.html.twig /var/www/my-project/app/Resources/views/base.html.twig;
-cp -ufr /var/www/react-app/dist/bundles/app/* /var/www/my-project/src/AppBundle/Resources/public;
-cd /var/www/my-project;
+cp /var/www/react-app/dist/base.html.twig /var/www/symfony-app/app/Resources/views/base.html.twig;
+cp -ufr /var/www/react-app/dist/bundles/app/* /var/www/symfony-app/src/AppBundle/Resources/public;
+cd /var/www/symfony-app;
 php bin/console assets:install;
 php bin/console cache:clear --env=prod --no-debug;
 rm -rf var/cache/prod;
